8eccffb1db98707a7159d98aed14db46507d3d28
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-02-21 11:39:07 -0600

    complete Queue class methods

--- queue.js
+++ queue.js
@@ -20,26 +20,39 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
-
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-
+    if (this.size === 0) throw Error('Queue is empty');
+    const first = this.first;
+    this.first = first.next;
+    this.size--;
+    return first.val;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.size === 0) return true;
+    return false;
   }
 }
 
